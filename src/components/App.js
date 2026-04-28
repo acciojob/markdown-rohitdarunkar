e9d88c983fd58d92ce1555c9b4aa6bc0044bcf5a
rohitdarunkar <rohitdarunkarwork@gmail.com>
2026-04-28 23:08:58 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,28 @@
-import React from "react";
-import "../styles/App.css";
-import MarkdownEditor from "./MarkdownEditor";
+import React, { useEffect, useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [markdown, setMarkdown] = useState("# Heading");
+
+  const convertMarkdown = (text) => {
+    return text
+      .split("\n")
+      .map((line, index) => {
+        if (line.startsWith("# ")) {
+          return <h1 key={index}>{line.replace("# ", "")}</h1>;
+        }
+        return <p key={index}>{line}</p>;
+      });
+  };
+
   return (
-    <div className="app">
-      {/* Do not remove the main div */}
-      <MarkdownEditor />
+    <div id="main">
+      <textarea
+        value={markdown}
+        onChange={(e) => setMarkdown(e.target.value)}
+      />
+
+      <div>{convertMarkdown(markdown)}</div>
     </div>
   );
 };
